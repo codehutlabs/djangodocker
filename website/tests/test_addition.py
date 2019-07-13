6e7488607589a9593cac6b06d4dc3c424606e151
Robert Kuzma <robert@balavec.com>
2019-07-13 13:10:58 +0200

    Update test_addition.py

--- website/tests/test_addition.py
+++ website/tests/test_addition.py
@@ -4,3 +4,7 @@ class TestUtils:
     def test_addition(self):
 
         assert 1 + 1 == 2, "1 + 1 should equal 2"
+
+    def test_multiplication(self):
+
+        assert 1 * 1 == 1, "1 * 1 should equal 1"
